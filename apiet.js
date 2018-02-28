a9d5f890438d1caa1e13f7fff5fb6c71a4e3f3bb
Cryptobath <33010722+Cryptobath@users.noreply.github.com>
2018-02-28 23:38:53 +0100

    fix

--- apiet.js
+++ apiet.js
@@ -6,7 +6,8 @@
                 "section": "ICO",
                 "value": 40,
                 "tagline" : "Amount of coins to sell in the ICO",
-                "description" : "The ICO begins in late April, the amount of coins to sell is 4 million (40% of total supply)." [
+                "description" : "The ICO begins in late April, the amount of coins to sell is 4 million (40% of total supply).",
+                "funds":[
                     {
                         "label" : "1. time period",
                         "url" : "https://www.google.com",
