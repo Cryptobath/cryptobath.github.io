8c04d02905e4bea13f718e2d457ddd6c94c7c7e9
Cryptobath <33010722+Cryptobath@users.noreply.github.com>
2018-03-01 14:52:46 +0100

    Update apiet.js

--- apiet.js
+++ apiet.js
@@ -8,21 +8,7 @@
                 "tagline" : "Amount of coins to sell in the ICO",
                 "description" : "The ICO begins in late April, the amount of coins to sell is 4 million (40% of total supply).",
                 "funds":[
-                    {
-                        "label" : "1. time period",
-                        "url" : "https://www.google.com",
-                        "shortlabel" : "2million"
-                    },
-                    {
-                        "label" : "2. time period",
-                        "url" : "https://www.google.com",
-                        "shortlabel" : "1million"
-                    },
-                    {
-                        "label" : "3. time period",
-                        "url" : "https://www.google.com",
-                        "shortlabel" : "1million"
-                    }
+                
                 ]
             },
             {
@@ -40,21 +26,7 @@
                 "tagline" : "The amount of coins that will be credited when claiming a Trust reward on the Trust platform.",
                 "description" : "The Trust platform will credit users that has over 1000 Trust points with 1 Droplet coin when claiming the Trust rewards, 1000:1 Ratio.",
                 "funds": [
-                    {
-                        "label" : "Blackrock Fund A",
-                        "url" : "https://www.google.com",
-                        "shortlabel" : "ABC"
-                    },
-                    {
-                        "label" : "iShares Fund B",
-                        "url" : "https://www.google.com",
-                        "shortlabel" : "ABC"
-                    },
-                    {
-                        "label" : "Blackrock Fund C",
-                        "url" : "https://www.google.com",
-                        "shortlabel" : "ABC"
-                    }
+                   
                 ]
             },
             {
@@ -63,21 +35,7 @@
                 "tagline" : "Coins sold in the pre-sale",
                 "description" : "The total amount of coins sold in the pre-sale, the remaining coins will be used for more Trust rewards.",
                 "funds": [
-                    {
-                        "label" : "Blackrock Fund A",
-                        "url" : "https://www.google.com",
-                        "shortlabel" : "ABC"
-                    },
-                    {
-                        "label" : "iShares Fund B",
-                        "url" : "https://www.google.com",
-                        "shortlabel" : "ABC"
-                    },
-                    {
-                        "label" : "Blackrock Fund C",
-                        "url" : "https://www.google.com",
-                        "shortlabel" : "ABC"
-                    }
+                   
                 ]
             }
 
